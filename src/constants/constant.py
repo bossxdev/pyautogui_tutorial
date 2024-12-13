b6a7a5f533141e12c4313801a35acb9ce20620e9
“Suphanu_Boss” <suphanut_boss@outlook.com>
2024-12-13 11:28:20 +0700

    Update CURRENT_DIR constant

--- src/constants/constant.py
+++ src/constants/constant.py
@@ -1,9 +1,12 @@
-# ค่าคงที่สำหรับโปรเจกต์
+import os
 
-# ค่าคงที่ชื่อโมดูล
+# ชื่อโมดูล
 MODULE_NAME = {
     "PYAUTOGUI_TUTORIAL": "pyautogui_tutorial",
 }
 
 # ค่าคงที่สำหรับการทดสอบ
 NSS_COMMENT_COLUMN = 18
+
+# ค่าคงที่สำหรับไดเรกทอรีพาธไฟล์
+CURRENT_DIR = os.path.dirname(__file__)
