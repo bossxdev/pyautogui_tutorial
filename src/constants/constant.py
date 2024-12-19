1913ce9c403f9c5a780b8ebd277ee51bb2c105f4
“Suphanu_Boss” <suphanut_boss@outlook.com>
2024-12-19 14:55:01 +0700

    Update constant.py

--- src/constants/constant.py
+++ src/constants/constant.py
@@ -3,5 +3,12 @@ MODULE_NAME = {
     "PYAUTOGUI_TUTORIAL": "pyautogui_tutorial",
 }
 
+# Path
+PATH = {
+    "ASSET": "../asset/",
+    "LOG": "../log/",
+}
+
 # ค่าคงที่สำหรับการทดสอบ
+NSS_EXCEL_FILE = "NSS-CS-POSNewGen-Plan240704.xlsx"
 NSS_COMMENT_COLUMN = 18
