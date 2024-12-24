ac493041c8133f45531c8fc4c07be7adc9a85281
NAPHADONHIPHIP <Nobphadonvud@gosoft.co.th>
2024-12-24 15:23:35 +0700

    [Fix] Edit service

--- src/scripts/(347)/วัดพระบาทน้ำพุ_TaxID.py
+++ src/scripts/(347)/วัดพระบาทน้ำพุ_TaxID.py
@@ -16,8 +16,7 @@ def setup_environment():
     log_handler("setup", "DEBUG", "เริ่มต้นการตั้งค่าระบบ...")
     time.sleep(2)
     log_handler("setup", "DEBUG", "การตั้งค่าเสร็จสิ้น")
-
-
+    
 def run_test_case():
     try:
         log_handler("test_case", "DEBUG", "เริ่มการทดสอบ...")
@@ -29,41 +28,39 @@ def run_test_case():
         pyautogui.keyDown('shift')
         pyautogui.press('\\')
         pyautogui.keyUp('shift')
-        
-        write_text("0993000070241", delay=2, description="กรอกบาร์โค้ด")
-        log_handler("test_case", "INFO", "กรอกบาร์โค้ด: 0993000070241")
+        write_text("3470000000000", delay=2, description="กรอกบาร์โค้ด")
+        log_handler("test_case", "INFO", "กรอกบาร์โค้ด: 3470000000000")
 
-        click((479, 456), delay=10, description="คลิกปุ่มยืนยัน pop up บาร์โค้ด")
+        click((490, 470), delay=30, description="คลิกปุ่มยืนยัน pop up บาร์โค้ด")
         log_handler("test_case", "INFO", "ยืนยัน pop up บาร์โค้ด")
+        
+        write_text("100", delay=3, description="กรอกจำนวนเงิน")
+        log_handler("test_case", "INFO", "กรอกจำนวนเงิน: 100")
 
-        click((370, 275), delay=30, description="คลิกเลือก service")
-        log_handler("test_case", "INFO", "เลือก service")
-
-        write_text("5000", delay=5, description="กรอกจำนวนเงิน")
-        log_handler("test_case", "INFO", "กรอกจำนวนเงิน: 5000")
-
-        click((668, 637), delay=5, description="กดปุ่มยืนยันทำรายการ")
+        
+       # flow การชำระเงิน
+        click((668, 637), delay=3, description="กดปุ่มยืนยันทำรายการ")
         log_handler("test_case", "INFO", "ยืนยันทำรายการ")
-
+        
         click((750, 636), delay=5, description="กดปุ่มยืนยันหน้าทวนข้อมูล")
         log_handler("test_case", "INFO", "ยืนยันหน้าทวนข้อมูล")
-
-
+        
         click((675, 636), delay=5, description="กดปุ่มชำระเงิน")
         log_handler("test_case", "INFO", "กดปุ่มชำระเงิน")
-
+        
         click((558, 633), delay=5, description="กดปุ่มรับพอดี")
         log_handler("test_case", "INFO", "กดปุ่มรับพอดี")
-
+        
         click((490, 430), delay=5, description="กดปุ่มยืนยันชำระเงิน")
         log_handler("test_case", "INFO", "ยืนยันชำระเงิน")
+        # flow การชำระเงิน
 
         log_handler("test_case", "DEBUG", "การทดสอบเสร็จสมบูรณ์")
-
+        
     except Exception as e:
         log_handler("test_case", "DEBUG", f"เกิดข้อผิดพลาดระหว่างการทดสอบ: {str(e)}")
         raise
-
+    
 
 if __name__ == "__main__":
     main()
