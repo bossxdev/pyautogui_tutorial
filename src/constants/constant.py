0a1472148a2a0a87ed150c3801f9b01f82c2a85e
“Suphanu_Boss” <suphanut_boss@outlook.com>
2024-12-26 11:08:03 +0700

    Update NSS_EXCEL_FILE constant

--- src/constants/constant.py
+++ src/constants/constant.py
@@ -2,10 +2,10 @@
 PATH = {
     "ASSET": "./src/asset/",
     "LOG": "./src/logs/",
-    "EXPORT": "./src/exports",
+    "EXPORT": "./src/exports"
 }
 
 # ค่าคงที่สำหรับการทดสอบ
-NSS_EXCEL_FILE = "NSS-CS-POSNewGen-Plan240704.xlsx"
+NSS_EXCEL_FILE = "[NSS-POSNewGen] Script Automate.xlsx"
 NSS_WORKSHEET_NAME = "pos_srv_new"
 NSS_COMMENT_COLUMN = 18
